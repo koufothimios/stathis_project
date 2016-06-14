c15c46786c88ab651bdca11e591e1b4f22e5cbf7
efthimis zeinis <e.zeinis@darkpony.com>
2016-06-14 17:23:59 +0300

    implemented on click stis photo

--- resources/views/includes/js.blade.php
+++ resources/views/includes/js.blade.php
@@ -3,3 +3,12 @@
 
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+
+    $('.col-md-4').on("click", "img", function () {
+        var source = $(this).attr('src');
+        $('#target_image').attr('src',source);
+    });
+
+</script>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,6 +11,11 @@
             </div>
             <div id="photo_section" class="col-md-10">
                 <div class="row">
+                    <div class="col-md-12">
+                        <img id="target_image" src="images/1.jpg">
+                    </div>
+                </div>
+                <div class="row">
                     <div class="col-md-4">
                         <img src="images/1.jpg">
                     </div>
