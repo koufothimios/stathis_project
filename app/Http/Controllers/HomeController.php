0a08e42e358e159c266bff43b66b42edf144521f
efthimis zeinis <e.zeinis@darkpony.com>
2016-06-17 12:18:50 +0300

    initialized seminaria page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -54,7 +54,19 @@ class HomeController extends Controller
     }
 
     public function seminaria(){
-        return view('pages.seminaria');
+        $loop_counter = 0;
+        $directory = public_path()."/images/seminaria/";
+        $vraveyseis_photos = glob($directory . "*.jpg");
+        $final_photos = [];
+        foreach($vraveyseis_photos as $photo){
+            $vraveyseis_photos[$loop_counter] = substr($photo,strlen(public_path()));
+            array_push($final_photos, explode("-",$vraveyseis_photos[$loop_counter]));
+            $final_photos[$loop_counter][0] = $vraveyseis_photos[$loop_counter];
+            $final_photos[$loop_counter][2] = substr($final_photos[$loop_counter][2], 0, -4);
+            $loop_counter++;
+        }
+        //return $final_photos;
+        return view('pages.vraveyseis',compact('final_photos'));
     }
 
     public function epikoinwnia(){
